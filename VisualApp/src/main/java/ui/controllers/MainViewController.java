1067954431f35c6d60a65e1c7e716be29a264d57
David González León <david.gonzalezleon@heig-vd.ch>
2021-01-24 18:19:16 +0100

    Minor changes main View and COntroller

--- VisualApp/src/main/java/ui/controllers/MainViewController.java
+++ VisualApp/src/main/java/ui/controllers/MainViewController.java
@@ -6,16 +6,31 @@
 package ui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import ui.Controller;
+import world.app.App;
 
 public class MainViewController extends Controller {
+   private Scene scene;
+   private App app;
+
    @FXML private TextFlow mainTextField;
 
    @Override
-   public void load(Stage primaryStage) {
+   public void setScene(Scene scene) {
+      this.scene = scene;
+   }
 
+   public void setApp(App app){
+      this.app = app;
+   }
+
+   @Override
+   public void load(Stage primaryStage) {
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Main menu");
    }
 
    @FXML
