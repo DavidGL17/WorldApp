ba81e82a02bcae8f117a900bd87b6446678532ac
David González León <david.gonzalezleon@heig-vd.ch>
2021-02-10 12:07:41 +0100

    Additional methods to App
    Added search by article type method, and a way to track the current selected world

--- backgroundApp/src/main/java/world/app/App.java
+++ backgroundApp/src/main/java/world/app/App.java
@@ -9,15 +9,18 @@ import util.HashMapChaining;
 import world.app.user.User;
 import world.app.world.Article;
 import world.app.world.World;
+import world.app.world.articles.TypeOfArticle;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 //Le launcher fournira toutes les méthodes nécessaire a l'interface graphique
 
 public class App {
    private User user;
+   private World currentWorld;
 
    public App(int userId, String password)
            throws User.WrongPasswordException, User.UserIdNotFoundException, SQLException {
@@ -51,5 +54,19 @@ public class App {
 
    public void addWorld(String name) {
       user.createWorld(name);
+   }
+
+   public void setCurrentWorld(World currentWorld) {
+      this.currentWorld = currentWorld;
+   }
+
+   public ArrayList<Article> getArticlesByArticleType(TypeOfArticle articleType) {
+      ArrayList<Article> articles = new ArrayList<>();
+      for (Article article : currentWorld.getArticles()) {
+         if (article.getTypeOfArticle() == articleType) {
+            articles.add(article);
+         }
+      }
+      return articles;
    }
 }
