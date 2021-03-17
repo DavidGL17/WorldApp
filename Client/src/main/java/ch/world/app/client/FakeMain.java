93b634d89b703b09ebfb9a192d4152f6a9ea3ab8
David González León <david.gonzalezleon@heig-vd.ch>
2021-03-17 23:58:08 +0100

    fixed packaging issue

---
+++ Client/src/main/java/ch/world/app/client/FakeMain.java
@@ -0,0 +1,14 @@
+/*
+ * @File FakeMain.java
+ * @Authors : David González León
+ * @Date 17 mars 2021
+ */
+package ch.world.app.client;
+
+import ch.world.app.client.visual.Launcher;
+
+public class FakeMain {
+   public static void main(String[] args) {
+      Launcher.main(args);
+   }
+}
