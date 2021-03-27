a031c5232f98a8a386cbbb422cba35e8961341d3
David González León <david.gonzalezleon@heig-vd.ch>
2021-03-27 19:30:25 +0100

    Added test to ErrorType and documentation

--- Util/src/main/java/ch/world/app/util/protocol/ErrorType.java
+++ Util/src/main/java/ch/world/app/util/protocol/ErrorType.java
@@ -5,7 +5,11 @@
  */
 package ch.world.app.util.protocol;
 
+/**
+ * The enum Error type, that describes the errors defined in the protocol.
+ */
 public enum ErrorType {
+
    SYSTEM_ERROR(500), WRONG_MESSAGE(501), SYNTAX_ERROR(400), REF_ERROR(404);
 
    private final int value;
@@ -14,7 +18,34 @@ public enum ErrorType {
       this.value = value;
    }
 
+   /**
+    * Gets the value of the ErrorType.
+    *
+    * @return the value
+    */
    public int getValue() {
       return value;
+   }
+
+   /**
+    * Gets the error type that coincides with the given value.
+    *
+    * @param value the value
+    *
+    * @return the error type with the value, or null if the value does not correspond to an ErrorType.
+    */
+   public static ErrorType getErrorTypeWithValue(int value) {
+      switch (value) {
+         case 400:
+            return SYNTAX_ERROR;
+         case 404:
+            return REF_ERROR;
+         case 500:
+            return SYSTEM_ERROR;
+         case 501:
+            return WRONG_MESSAGE;
+         default:
+            return null;
+      }
    }
 }
