8bc257b679e114fc0f940ef24f1477f50c8d11be
David González León <david.gonzalezleon@heig-vd.ch>
2021-05-15 17:48:58 +0200

    last server update, before switching to http

--- Server/src/main/java/ch/world/app/server/WorldAppServer.java
+++ Server/src/main/java/ch/world/app/server/WorldAppServer.java
@@ -6,9 +6,11 @@
 package ch.world.app.server;
 
 import ch.world.app.util.Logging;
+import ch.world.app.util.protocol.WorldAppProtocol;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -18,6 +20,8 @@ import java.util.logging.Logger;
 public class WorldAppServer {
    private final Logger logger;
    private final List<Worker> activeWorkers;
+   private ServerSocket serverSocket;
+   private boolean shouldRun = true;
 
    public WorldAppServer() throws IOException {
       //Setup Logger
@@ -36,14 +40,40 @@ public class WorldAppServer {
    }
 
    public void start() {
-      logger.info("Starting the server...");
+      try {
+         logger.log(Level.INFO, "Starting the server on port {0}...", WorldAppProtocol.DEFAULT_PORT);
+         serverSocket = new ServerSocket(WorldAppProtocol.DEFAULT_PORT);
+         while (shouldRun) {
+            Socket clientSocket = serverSocket.accept();
+            Worker worker = new Worker(clientSocket);
+            registerWorker(worker);
+            new Thread(worker).start();
+         }
+         serverSocket.close();
+         logger.info("shouldRun is false... server going down");
 
+      } catch (IOException e) {
+         logger.log(Level.SEVERE, e.getMessage(), e);
+         e.printStackTrace();
+         System.exit(-1);
+      }
+   }
+
+   private void shutdown() {
+      logger.info("Shutting down server...");
+      shouldRun = false;
+      try {
+         serverSocket.close();
+      } catch (IOException ex) {
+         logger.log(Level.SEVERE, ex.getMessage(), ex);
+      }
+      disconnectConnectedWorkers();
    }
 
    private void registerWorker(Worker worker) {
-      logger.log(Level.INFO, ">> Waiting for lock before registring worker {0}", worker.userId);
+      logger.log(Level.INFO, ">> Waiting for lock before registring new worker...");
       activeWorkers.add(worker);
-      logger.log(Level.INFO, "<< Worker {0} registered.", worker.userId);
+      logger.log(Level.INFO, "<< New Worker registered.");
    }
 
    private void unregisterWorker(Worker worker) {
@@ -52,13 +82,77 @@ public class WorldAppServer {
       logger.log(Level.INFO, "<< Worker {0} unregistered.", worker.userId);
    }
 
+   private void disconnectConnectedWorkers() {
+      logger.info(">> Waiting for lock before disconnecting workers");
+      synchronized (activeWorkers) {
+         logger.info("Disconnecting workers");
+         for (Worker worker : activeWorkers) {
+            worker.disconnect();
+         }
+      }
+      logger.info("<< Workers disconnected");
+   }
+
    class Worker implements Runnable {
 
+      private final Socket userSocket;
+      private BufferedReader in;
+      private PrintWriter out;
       private int userId;
+      private boolean connected = true;
+
+      Worker(Socket userSocket) {
+         this.userSocket = userSocket;
+         try {
+            in = new BufferedReader(new InputStreamReader(userSocket.getInputStream()));
+            out = new PrintWriter(userSocket.getOutputStream());
+            connected = true;
+         } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+         }
+      }
 
       @Override
       public void run() {
 
+         while (connected) {
+
+         }
+      }
+
+      private void cleanup() {
+         logger.log(Level.INFO, "Cleaning up worker used by {0}", userId);
+
+         logger.log(Level.INFO, "Closing clientSocket used by {0}", userId);
+         try {
+            if (userSocket != null) {
+               userSocket.close();
+            }
+         } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+         }
+
+         logger.log(Level.INFO, "Closing in used by {0}", userId);
+         try {
+            if (in != null) {
+               in.close();
+            }
+         } catch (IOException ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+         }
+
+         logger.log(Level.INFO, "Closing out used by {0}", userId);
+         if (out != null) {
+            out.close();
+         }
+
+         logger.log(Level.INFO, "Clean up done for worker used by {0}", userId);
+      }
+
+      private void disconnect() {
+         logger.log(Level.INFO, "Disconnecting worker used by {0}", userId);
+         connected = false;
+         cleanup();
       }
    }
 }
---
+++ Util/src/main/java/ch/world/app/util/protocol/WorldAppProtocol.java
@@ -0,0 +1,11 @@
+/*
+ * @File WorldAppProtocol.java
+ * @Authors : David González León
+ * @Date 28 mars 2021
+ */
+package ch.world.app.util.protocol;
+
+public class WorldAppProtocol {
+   public static final int DEFAULT_PORT = 8951;
+   public static final String DEFAULT_ADDRESS = "TBD";
+}
