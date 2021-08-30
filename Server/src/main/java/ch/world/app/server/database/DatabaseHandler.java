6e89e6fd33d3efe8ce61d70d3eb713fdf7765d14
David Gonzalez Leon <david.gonzalez@citycable.ch>
2021-08-30 17:57:37 +0200

    added databasehandler

---
+++ Server/src/main/java/ch/world/app/server/database/DatabaseHandler.java
@@ -0,0 +1,11 @@
+/*
+ * @File DatabaseHandler.java
+ * @Authors : David González León
+ * @Date 17 mai 2021
+ */
+package ch.world.app.server.database;
+
+public class DatabaseHandler {
+   //TODO handles connection to database and upload/creation/deletion of content. handles parsing from json to the
+   // database
+}
